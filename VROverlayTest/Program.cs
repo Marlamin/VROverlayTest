13508a6cb18988117bb5fbb42419701b74c6237b
Martin Benjamins <marlamin@marlamin.com>
2016-04-26 23:53:34 +0200

    Initial try at messing around

---
+++ VROverlayTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+using Valve.VR;
+
+namespace VROverlayTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var vr = SteamVR.instance;
+            //Console.WriteLine(vr.hmd_ModelNumber);
+        }
+    }
+}
